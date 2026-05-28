1daf35a0951dee131081859178dc72899efd7a1b
AldaAtitus <1137226@atitus.edu.br>
2026-05-27 21:50:19 -0300

    Copia e Cola1

--- src/main/java/br/edu/atitus/aldacir_stanguerlin_junior/zoo_digital/animais/Cachorro.java
+++ src/main/java/br/edu/atitus/aldacir_stanguerlin_junior/zoo_digital/animais/Cachorro.java
@@ -1,4 +1,34 @@
 package br.edu.atitus.aldacir_stanguerlin_junior.zoo_digital.animais;
 
-public class Cachorro {
+import br.edu.atitus.aldacir_stanguerlin_junior.zoo_digital.comportamentos.*;
+import br.edu.atitus.aldacir_stanguerlin_junior.zoo_digital.especies.*;
+//import br.edu.atitus.interfaces.Nado;
+
+public final class Cachorro extends Mamifero implements Corredor, Nadador{
+    public Cachorro(String nome, Integer idade) {
+        super(nome, idade, true);
+        
+        
+    }
+
+    @Override
+    public void comer() {
+        this.comer("ração");
+    }
+
+    @Override
+    public void emitirSom() {
+        IO.println(this.getNome() + " está latindo!");
+    }
+
+    @Override
+    public void nadar() {
+        IO.println(this.getNome() + " está nadando estilo cachorrinho!");
+    }
+
+    @Override
+    public void correr() {
+        IO.println(this.getNome() + " está correndo por todo o pátio!");
+    }
+    
 }
--- src/main/java/br/edu/atitus/aldacir_stanguerlin_junior/zoo_digital/comportamentos_interfaces/Predador.java
+++
@@ -1,4 +0,0 @@
-package br.edu.atitus.aldacir_stanguerlin_junior.zoo_digital.comportamentos_interfaces;
-
-public interface Predador {
-}
