7000a2409718a9f90f0d0db81d3261dfeeded5e3
AldaAtitus <1137226@atitus.edu.br>
2026-05-29 21:52:29 -0300

    System.out porque IO não funciona2

--- src/main/java/br/edu/atitus/aldacir_stanguerlin_junior/zoo_digital/app/ZooDigitalApp.java
+++ src/main/java/br/edu/atitus/aldacir_stanguerlin_junior/zoo_digital/app/ZooDigitalApp.java
@@ -3,7 +3,6 @@ package br.edu.atitus.aldacir_stanguerlin_junior.zoo_digital.app;
 import br.edu.atitus.aldacir_stanguerlin_junior.zoo_digital.especies.*;
 import br.edu.atitus.aldacir_stanguerlin_junior.zoo_digital.animais.*;
 import br.edu.atitus.aldacir_stanguerlin_junior.zoo_digital.comportamentos.*;
-import br.edu.atitus.aldacir_stanguerlin_junior.zoo_digital.especies.IO;
 
 import java.util.List;
 import java.util.ArrayList;
