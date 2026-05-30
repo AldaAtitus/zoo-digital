246f936a3ead284b9f640e40412a61765002c31d
AldaAtitus <1137226@atitus.edu.br>
2026-05-29 21:51:28 -0300

    System.out porque IO não funciona

---
+++ src/main/java/br/edu/atitus/aldacir_stanguerlin_junior/zoo_digital/especies/IO.java
@@ -0,0 +1,14 @@
+package br.edu.atitus.aldacir_stanguerlin_junior.zoo_digital.especies;
+
+public final class IO {
+    private IO() {
+    }
+
+    public static void print(String mensagem) {
+        System.out.print(mensagem);
+    }
+
+    public static void println(String mensagem) {
+        System.out.println(mensagem);
+    }
+}
